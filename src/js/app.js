f0056bfc13f27a0cf39059f1b4ab779c2d62eddc
besailazi <126988974+besailazi@users.noreply.github.com>
2024-02-15 11:09:31 +0100

    add pro member and eventlistener

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 const firstnameInput = document.querySelector('.firstname');
 const lastnameInput = document.querySelector('.lastname');
 const phoneInput = document.querySelector('.phonenumber');
+const isProInput = document.querySelector('.pro-member');
 
 const submitButton = document.querySelector('.submit-button');
 
@@ -12,11 +13,35 @@ class Member {
 	constructor(firstname, lastname, phonenumber){
 		this.firstname = firstname
 		this.lastname = lastname
-		this.phone = phonenumber
+		this.phonenumber = phonenumber
 	}
 
-	createMember() {
-		const newMember = new Member(firstnameInput.value, lastnameInput.value, phoneInput.value)
+	static createMember() {
+		const newMember = new Member(firstnameInput.value, lastnameInput.value, phoneInput.value);
 		return newMember;
 	}
 }
+
+class ProMember extends Member {
+	constructor(firstname, lastname, phonenumber, isProMember){
+		super(firstname, lastname, phonenumber)
+		this.isProMember = isProMember
+	}
+
+	static createProMember(){
+		const newProMember = new ProMember(firstnameInput.value, lastnameInput.value, phoneInput.value, isProInput.checked);
+		return  newProMember;
+	}
+}
+
+submitButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+	 if(isProInput.checked){
+		allMembers.push(ProMember.createProMember());
+		
+	 }else{
+		allMembers.push(Member.createMember());
+	 }
+
+	 console.log(allMembers);
+})
