cf150bbcd55557edfd0825cc816a9d6c76df4d9f
besailazi <126988974+besailazi@users.noreply.github.com>
2024-02-15 10:44:20 +0100

    add member class

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-alert(hi);
+// GET DOM ELEMENTS
+
+const firstnameInput = document.querySelector('.firstname');
+const lastnameInput = document.querySelector('.lastname');
+const phoneInput = document.querySelector('.phonenumber');
+
+const submitButton = document.querySelector('.submit-button');
+
+allMembers = [];
+
+class Member {
+	constructor(firstname, lastname, phonenumber){
+		this.firstname = firstname
+		this.lastname = lastname
+		this.phone = phonenumber
+	}
+
+	createMember() {
+		const newMember = new Member(firstnameInput.value, lastnameInput.value, phoneInput.value)
+		return newMember;
+	}
+}
